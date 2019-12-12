028dabc2a6557ff5b990ef7804c79fc6c5a9b428
Theodore Nguyen <theodore.a.nguyen@gmail.com>
2019-12-11 17:58:21 -0800

    Add files via upload

---
+++ color.py
@@ -0,0 +1,13 @@
+#https://stackoverflow.com/questions/8924173/how-do-i-print-bold-text-in-python
+#This class and the code below it is used for text formatting
+
+PURPLE = '\033[95m'
+CYAN = '\033[96m'
+DARKCYAN = '\033[36m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+END = '\033[0m'
